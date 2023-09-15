ec4f0b5ad8b282de3789167608f5374c65c0b7e7
Prem Kumar  Chelamalasetti <pchelamalasetti@innominds.com>
2023-09-15 21:31:54 +0530

    Changes Made

--- src/main/java/com/userapi/UserapiApplication.java
+++ src/main/java/com/userapi/UserapiApplication.java
@@ -3,11 +3,16 @@ package com.userapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class UserapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserapiApplication.class, args);
+    }
+
 
 }
